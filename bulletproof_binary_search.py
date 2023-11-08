5675cd81a873cf57d7f918a5283b9c145d1b259c
Adit Chakraborty <aditchakraborty2009@gmail.com>
2023-11-08 15:14:36 +0800

    Create bulletproof_binary_search

---
+++ bulletproof_binary_search.py
@@ -0,0 +1,26 @@
+def binary_search(n, t, r):
+    low = 0
+    mid = 0
+    high = len(n) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if n[mid] < t:
+            low = mid + 1
+        elif n[mid] > t:
+            high = mid - 1
+        else:
+            return mid
+
+    if r == 0:
+        return -1
+    else:
+        return binary_search(n, t, r-1)
+
+
+def bulletproof_binary_search(n, t, r):
+    return binary_search(n, t, 2)
+
+
+print(bulletproof_binary_search([2, 3, 4, 10, 40], 10, 2))
