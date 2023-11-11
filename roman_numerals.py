d2f2de0dbd117e1ae49cefa4fe769adf87162a77
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-11-11 12:14:20 +0800

    rename function

--- roman_numerals.py
+++ roman_numerals.py
@@ -11,50 +11,50 @@ def roman_to_decimal(n):
 
     return result
 
-def print_roman(num):
+def decimal_to_roman(num):
     result = ""
     if num >= 1000:
         result += "M"
-        result += print_roman(num - 1000)
+        result += decimal_to_roman(num - 1000)
     elif num >= 900:
         result += "CM"
-        result += print_roman(num - 900)
+        result += decimal_to_roman(num - 900)
     elif num >= 500:
         result += "D"
-        result += print_roman(num - 500)
+        result += decimal_to_roman(num - 500)
     elif num >= 400:
         result += "CD"
-        result += print_roman(num - 400)
+        result += decimal_to_roman(num - 400)
     elif num >= 100:
         result += "C"
-        result += print_roman(num - 100)
+        result += decimal_to_roman(num - 100)
     elif num >= 90:
         result += "XC"
-        result += print_roman(num - 90)
+        result += decimal_to_roman(num - 90)
     elif num >= 50:
         result += "L"
-        result += print_roman(num - 50)
+        result += decimal_to_roman(num - 50)
     elif num >= 40:
         result += "XL"
-        result += print_roman(num - 40)
+        result += decimal_to_roman(num - 40)
     elif num >= 10:
         result += "X"
-        result += print_roman(num - 10)
+        result += decimal_to_roman(num - 10)
     elif num >= 9:
         result += "IX"
-        result += print_roman(num - 9)
+        result += decimal_to_roman(num - 9)
     elif num >= 5:
         result += "V"
-        result += print_roman(num - 5)
+        result += decimal_to_roman(num - 5)
     elif num >= 4:
         result += "IV"
-        result += print_roman(num - 4)
+        result += decimal_to_roman(num - 4)
     elif num >= 1:
         result += "I"
-        result += print_roman(num - 1)
+        result += decimal_to_roman(num - 1)
     
     return result
 
 print(roman_to_decimal("IIX"))
-print(print_roman(12))
+print(decimal_to_roman(12))
     
