d8e57b04c9ba657594bb618271fbabd5d6f5c373
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-11-24 12:38:13 +0800

    do variable machine

---
+++ variable_machine.py
@@ -0,0 +1,4 @@
+CelebrityAge = int(input('When was the celebrity born?'))
+MyAge = int(input('When were you born?'))
+AgeDifference = MyAge - CelebrityAge
+print("The celebrity is ",AgeDifference," years older than you")
