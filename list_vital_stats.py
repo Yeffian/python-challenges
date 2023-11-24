5e9e5633224a9c9b534672339b7a2a0d28252ac9
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-11-24 14:32:33 +0800

    add list vitals

---
+++ list_vital_stats.py
@@ -0,0 +1,7 @@
+list = [i for i in range(1, 101)]
+
+print('Mean:', sum(list) / len(list))
+print('Length:', len(list))
+print('Maximum:', max(list))
+print('Minimum:', min(list))
+print('Total:', sum(list))
