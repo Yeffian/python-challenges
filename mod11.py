326d89f97681b57a63fb7ac5ecfb5bb25718092a
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-11-24 14:47:25 +0800

    add code for mod11

---
+++ mod11.py
@@ -0,0 +1,11 @@
+def calculate_check_digit(isbn_n):
+    isbn_sums = []
+    mul_nums = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+    for i in range(len(str(isbn_n)) - 1):
+        # print(int(str(isbn_n)[i]) * mul_nums[i])
+        isbn_sums.append(int(str(isbn_n)[i]) * mul_nums[i])
+    
+    modded_sum = sum(isbn_sums) % 11
+    
+    return 11 - modded_sum
